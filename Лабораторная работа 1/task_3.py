bf723404d039938efc2094ea1cd777222903fa2d
Krytanyc <147938106+Krytanyc@users.noreply.github.com>
2023-10-14 19:41:39 +0300

    Лабораторная работа 1

---
+++ Лабораторная работа 1/task_3.py
@@ -0,0 +1,8 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+c = len(list_players)
+b = list_players[0:c//2]
+a = list_players[c//2:c]
+print(b)
+print(a)
+
+# TODO Разделите участников на две команды
